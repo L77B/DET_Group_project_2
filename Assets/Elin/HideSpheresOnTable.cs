41ffabacc7e869abdb3b1d72b5f4e1f0c9ca3911
L77B <113351312+L77B@users.noreply.github.com>
2026-03-11 15:34:02 +0100

    fixes

--- Assets/Elin/HideSpheresOnTable.cs
+++ Assets/Elin/HideSpheresOnTable.cs
@@ -4,30 +4,50 @@ public class HideSpheresOnTable : MonoBehaviour
 {
     public GameObject[] scaleSpheres;   //This creates the Inspector slots
     public string tableTag = "Table";
+    public int originalLayer;
     private Rigidbody rb;
-    private void Start() 
-    { 
-        rb = GetComponent<Rigidbody>(); 
+    private Rigidbody touchedWallRb;
+    private bool touchedWallWasKinematic;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        originalLayer = rb.gameObject.layer;
     }
+
+
+
+    public void ChangeObjectLayer(int newLayer)
+    {
+        gameObject.layer = newLayer;
+
+        int childCount = transform.childCount;
+        for (int child = 0; child < childCount; child++)
+        {
+            transform.GetChild(child).gameObject.layer = newLayer;
+        }
+    }
+
 
     private void OnCollisionEnter(Collision collision)
-{
-    if (collision.collider.CompareTag(tableTag))
     {
-        // Hide spheres 
-        foreach (var sphere in scaleSpheres) 
-        sphere.SetActive(false);
-        // Snap upright
-        Vector3 euler = transform.eulerAngles;
-        euler.x = 0f;
-        euler.z = 0f;
-        transform.eulerAngles = euler;
 
-        // Optional: re-enable physics stability
-        rb.angularVelocity = Vector3.zero;
+        if (collision.collider.CompareTag(tableTag))
+        {
+
+            //gameObject.layer = originalLayer;
+
+            // Hide spheres
+            foreach (var sphere in scaleSpheres)
+                sphere.SetActive(false);
+
+            // Snap upright
+            Vector3 euler = transform.eulerAngles;
+            euler.x = 0f;
+            euler.z = 0f;
+            transform.eulerAngles = euler;
+        }
+
     }
-}
-
 
     private void OnCollisionExit(Collision collision)
     {
